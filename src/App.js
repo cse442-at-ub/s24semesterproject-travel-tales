c60c0778d059f9197c55ab72bc0da30b13876c79
Jgoss02 <jgoss2@buffalo.edu>
2024-02-17 23:41:46 -0500

    Added what was needed for production

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import React, { useEffect, useState } from 'react';
 function App() {
 
   const [backendMessage, setBackendMessage] = useState('');
-  useEffect(() => {
-    // Fetch data from your PHP backend
-    fetch('index.php')
-      .then(response => response.text())
-      .then(data => {
-        setBackendMessage(data); // Set the response data to state
-      })
-      .catch(error => console.error('There was an error!', error));
-  }, []);
+    useEffect(() => {
+        // Fetch data from your PHP backend
+        //fetch('index.php')
+        //console.log(process.env.REACT_APP_API_BASE_URL);
+        fetch(`${ process.env.REACT_APP_API_BASE_URL }`)
+            .then(response => response.text())
+            .then(data => {
+                setBackendMessage(data); // Set the response data to state
+            })
+            .catch(error => console.error('There was an error!', error));
+    }, []);
 
   return (
     <div className="App">
