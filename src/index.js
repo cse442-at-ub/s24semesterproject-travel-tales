ce8d1bee79e82a1466cd7c174a1da7f48f773252
Colemanzi <Colemanz@buffalo.edu>
2024-02-20 20:59:54 -0500

    Created google maps api, able to place and remove

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '../../s24semesterproject-travel-tales/src/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
