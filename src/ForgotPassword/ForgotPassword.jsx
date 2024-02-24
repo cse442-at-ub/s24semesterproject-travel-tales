aa3594bcec4253ad9deda242164210e8e43b6314
Joshuas7330 <jsit@buffalo.edu>
2024-02-23 19:22:51 -0500

    Added simple input dection to check if user input anything into text box and check if emails are valid when button is pressed. #46

--- src/ForgotPassword/ForgotPassword.jsx
+++ src/ForgotPassword/ForgotPassword.jsx
@@ -5,11 +5,32 @@ import BannerImage from "../Assets/Login/Background.png"
 
 export const ForgotPassword = (props) => {
     const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
     }
+
+    const onButtonClick = () => {
+        setEmailError('')
+
+        // Check if the user has entered both fields correctly
+        if ('' === email) {
+            setEmailError('Please enter your email')
+            return
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            setEmailError('Please enter a valid email')
+            return
+        }
+    }
+
+
+
 
     return (
         <div className="Forgot-Password">
@@ -18,8 +39,9 @@ export const ForgotPassword = (props) => {
                 <h1 className ="text">Enter the email associated with your account and we will send you a code to reset your password</h1>
                 <label htmlFor="email"></label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" id="email" name="email" />
+                <label className="errorLabel">{emailError}</label>
+                <button className="confirm" onClick={onButtonClick}>Continue</button>
 
-                <button className="confirm">Continue</button>
                 <Link className="link" to="/Login" >Go Back?</Link>
             </form>
       
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -8,11 +8,35 @@ import Travel_Tales from "../Assets/Login/Title Section.png"
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [emailError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
     }
+    const onButtonClick = () => {
+        setEmailError('')
+        setPasswordError('')
+
+        // Check if the user has entered both fields correctly
+        if ('' === email) {
+            setEmailError('Please enter your email')
+            
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            setEmailError('Please enter a valid email')
+            
+        }
+
+        if ('' === pass) {
+            setPasswordError('Please enter a password')
+            
+        }
+        return
+    }
+
 
     return (
        
@@ -26,11 +50,11 @@ export const Login = (props) => {
 
                 <label htmlFor="email"></label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" id="email" name="email" />
-
+                    <label className="errorLabel">{emailError}</label>
                 <label htmlFor="password"></label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
-
-                <button className="login-button">Log In</button>
+                    <label className="errorLabel">{passwordError}</label>
+                    <button className="login-button" onClick={onButtonClick}>Log In</button>
                 <Link className="link" to="/ForgotPassword" >Forgot Password?</Link>
                 <Link className="link" to="/Register" >Create an account</Link>
 
