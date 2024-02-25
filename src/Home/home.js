90a6b4962e4e8d5267b05d73463fc6d12708559e
Colemanzi <Colemanz@buffalo.edu>
2024-02-24 22:03:57 -0500

    minor change before testing t#53

--- src/Home/home.js
+++ src/Home/home.js
@@ -154,7 +154,7 @@ const App = () => {
               <button className="switch" onClick={handleToggleClick}>
                 {isToggled ? <ToggleOnIcon fontSize='large' /> : <ToggleOffIcon fontSize='large' color='disabled' />}
               </button>
-              <Button className="add-pin-box" variant="contained" onClick={(handleSubmit)} style={{ borderRadius: 20 }}>Add Pin</Button>           
+              <Button className="add-pin-box" variant="contained" onClick={(handleSubmit)} style={{ borderRadius: 10 }}>Add Pin</Button>           
             </Box>
           </Modal>
         </header>  
