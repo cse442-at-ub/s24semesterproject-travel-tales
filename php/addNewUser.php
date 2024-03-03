a885d95adc97d56a7654c2d096a4621e44379fd1
Devon Morlok <72158862+DevonMorlok@users.noreply.github.com>
2024-03-03 14:59:18 -0500

    Added additional password requirements on account creation t#52

--- php/addNewUser.php
+++ php/addNewUser.php
@@ -1,5 +1,7 @@
-// insertData.php
 <?php
+
+include_once('db.php');
+header('Content-Type: application/json');
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
@@ -18,17 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 $data = json_decode(file_get_contents("php://input"), true);
 $firstName = $data['firstName'];
 $lastName = $data['lastName'];
@@ -36,15 +27,61 @@ $email = $data['email'];
 $pass = $data['pass'];
 $confirmPass = $data['confirmPass'];
 
-if ($pass !== $confirmPass) { // check if passwords match
-    die(json_encode(['error' => 'Passwords do not match.']));
+// Check if a user already exists
+$check_if_exists = "SELECT email FROM users WHERE email = ?";
+$stmt = $conn->prepare($check_if_exists);
+
+$stmt->bind_param("s", $email);
+$stmt->execute();
+
+$user_exists = $stmt->get_result();
+if ($user_exists->num_rows > 0) { // Email is already in the DB
+    http_response_code(400);
+    die(json_encode(['message' => 'An account with this email already exists.']));
+} 
+
+// check if passwords match
+if ($pass !== $confirmPass) {
+    http_response_code(400);
+    die(json_encode(['message' => 'Passwords do not match.']));
 }
 
-if (strlen($pass) < 8) { // check if password is at least 8 characters
-    die(json_encode(['error'=> 'Password must be at least 8 characters long.']));
+// check if password is at least 8 characters
+if (strlen($pass) < 8) {
+    http_response_code(400);
+    die(json_encode(['message' => 'Password must be at least 8 characters long.']));
 }
+
+// check if password contains at least one uppercase letter
+$noUppercase = !preg_match('/[A-Z]/', $pass);
+if ($noUppercase) {
+    http_response_code(400);
+    die(json_encode(['message' => 'Password requires at least 1 uppercase letter.']));
+}
+
+// check if password contains at least one lowercase letter
+$noLowercase = !preg_match('/[a-z]/', $pass);
+if ($noLowercase) {
+    http_response_code(400);
+    die(json_encode(['message' => 'Password requires at least 1 lowercase character.']));
+}
+
+// check to make sure there is at least one special character
+$pattern = '/[^a-zA-Z0-9]/';
+if (!preg_match($pattern, $pass)) {
+    http_response_code(400);
+    die(json_encode(['message' => 'Password requires at least 1 special character.']));
+}
+
+// check if the password contains a number
+$pattern = '/\d/';
+if (!preg_match($pattern, $pass)) {
+    http_response_code(400);
+    die(json_encode(['message' => 'Password requires at least one number.']));
+}
+
 // hash and salt password
-$pass = password_hash($data['password'], PASSWORD_DEFAULT);
+$pass = password_hash($pass, PASSWORD_DEFAULT);
 
 if(isset($email) && isset($pass)) {
     if($email != "" and $pass != ""){
@@ -52,14 +89,16 @@ if(isset($email) && isset($pass)) {
         $stmt->bind_param("ssss", $firstName, $lastName, $email, $pass);
         
         if($stmt->execute()) {
-            echo json_encode(["message" => "Record inserted successfully"]);
+            http_response_code(201);
+            echo json_encode(['message' => 'Record inserted successfully']);
         } else {
-            echo json_encode(["message" => "Failed to insert record"]);
+            http_response_code(500);
+            echo json_encode(['message' => 'Failed to insert record']);
         }
     }
 } else {
-    echo json_encode(["message" => "Please provide username and email"]);
+    http_response_code(400);
+    echo json_encode(['message' => 'Please provide username and email']);
 }
 
 $conn->close();
-?>
