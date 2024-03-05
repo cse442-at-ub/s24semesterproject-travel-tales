96bdd4563775dbbe31fe303fc7ab4cc896ccbb3f
Colemanzi <Colemanz@buffalo.edu>
2024-03-05 18:59:05 -0500

    changed to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import React, { useState } from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RequireAuth from './Auth/RequireAuth.jsx';
 import { Login } from "./Login/Login.jsx";
 import { ForgotPassword } from "./ForgotPassword/ForgotPassword.jsx";
 import {Register} from "./Register/Register.jsx";
 import Home from './Home/home.js';
 import './App.css';
-
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<Register />} />
-          <Route path="/forgotPassword" element={<ForgotPassword />} />
-          <Route path="/" element={
-            <RequireAuth>
-              <Home/>
-            </RequireAuth>
-          } />
-        </Routes>         
-      </Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/" element={<RequireAuth><Home /></RequireAuth>} />
+      </Routes>
+    </Router>
   );
-  
 }
 
 export default App;
