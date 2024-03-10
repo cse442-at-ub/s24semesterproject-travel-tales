ff53f37f0f4fa1c7d53f5b3fc5e8ecd132979dd4
joshuas7330 <jsit@buffalo.edu>
2024-03-10 17:13:42 -0400

    t#68

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" element={
             <RequireAuth>
               <Home/>
-          //  </RequireAuth>
+           </RequireAuth>
           } />
         </Routes>         
       </Router>
