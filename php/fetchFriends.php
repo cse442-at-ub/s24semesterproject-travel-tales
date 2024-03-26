63d6b15b719a16bfaaa83792fa0c217f9d706ae4
Colemanzi <Colemanz@buffalo.edu>
2024-03-25 20:30:04 -0400

    updated to HTTPS header t#82

--- php/fetchFriends.php
+++ php/fetchFriends.php
@@ -1,15 +1,21 @@
 <?php
-
 session_start();
 require_once 'db.php';
 
 $friendsList = [];
-// Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+//forces HTTPS
+if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+}
+if (isset($_SERVER['HTTPS_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTPS_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400'); // cache for 1 day
 }
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true'); 
+header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     
@@ -68,3 +74,4 @@ if (isset($_SESSION['user_id'])) {
 } else {
     echo "Please log in to view friends.";
 }
+$conn->close();
