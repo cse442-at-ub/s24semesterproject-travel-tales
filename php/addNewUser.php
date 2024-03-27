90f9b9ce95a3b898cfe0eec4478ab0b7de0c854d
Devon Morlok <72158862+DevonMorlok@users.noreply.github.com>
2024-03-27 19:40:15 -0400

    Added basic Username handling t#91

--- php/addNewUser.php
+++ php/addNewUser.php
@@ -29,10 +29,11 @@ $data = json_decode(file_get_contents("php://input"), true);
 $firstName = $data['firstName'];
 $lastName = $data['lastName'];
 $email = $data['email'];
+$username = $data['username'];
 $pass = $data['pass'];
 $confirmPass = $data['confirmPass'];
 
-// Check if a user already exists
+// Check if a email already exists
 $check_if_exists = "SELECT email FROM users WHERE email = ?";
 $stmt = $conn->prepare($check_if_exists);
 
@@ -43,6 +44,19 @@ $user_exists = $stmt->get_result();
 if ($user_exists->num_rows > 0) { // Email is already in the DB
     http_response_code(400);
     die(json_encode(['message' => 'An account with this email already exists.']));
+}
+
+// Check if a username already exists
+$check_username = "SELECT username FROM users WHERE username = ?";
+$stmt = $conn->prepare($check_username);
+
+$stmt->bind_param("s", $username);
+$stmt->execute();
+
+$user_exists = $stmt->get_result();
+if ($user_exists->num_rows > 0) { // Username is already in the DB
+    http_response_code(400);
+    die(json_encode(['message' => 'An account with this username already exists.']));
 } 
 
 // check if passwords match
@@ -90,8 +104,8 @@ $pass = password_hash($pass, PASSWORD_DEFAULT);
 
 if(isset($email) && isset($pass)) {
     if($email != "" and $pass != ""){
-        $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, pass) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $firstName, $lastName, $email, $pass);
+        $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, username, pass) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $firstName, $lastName, $email, $username, $pass);
         
         if($stmt->execute()) {
             http_response_code(201);
