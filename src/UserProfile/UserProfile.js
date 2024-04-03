c0b4d0d33a0909e674a47decc9fb86b943d42c1f
Joshuas7330 <90070746+Joshuas7330@users.noreply.github.com>
2024-04-03 01:47:56 -0400

    Bug Fix t#95

--- src/UserProfile/UserProfile.js
+++ src/UserProfile/UserProfile.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import UsernameForm from '../Components/UsernameForm';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -26,228 +25,245 @@ import SettingsDialog from '../Settings/SettingsDialog';
 
 
 const SwipeableTemporaryDrawer = ({ open, onClose }) => {
-const [drawerAnchor, setDrawerAnchor] = useState('right');
-const [myPinsModalOpen, setMyPinsModalOpen] = useState(false);
-const [FriendsModalOpen, setFriendsModalOpen] = useState(false);
-const [profileData, setProfileData] = useState('black')
-const email = localStorage.getItem('email');
+    const [drawerAnchor, setDrawerAnchor] = useState('right');
+    const [myPinsModalOpen, setMyPinsModalOpen] = useState(false);
+    const [FriendsModalOpen, setFriendsModalOpen] = useState(false);
+    const [profileData, setProfileData] = useState('black')
+    const email = localStorage.getItem('email');
 
 
-useEffect(() => {
-    const handleFetchProfile = async () => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/Profile.php?email=${email}`);
-            const data = await response.json();
-            setProfileData(data.profile)
+    useEffect(() => {
+        const handleFetchProfile = async () => {
+            try {
+                const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/Profile.php?email=${email}`);
+                const data = await response.json();
+                setProfileData(data.profile)
 
-        } catch (error) {
-            console.error('Error fetching profile:', error);
+            } catch (error) {
+                console.error('Error fetching profile:', error);
+            }
+        };
+        handleFetchProfile();
+
+    }, [email]);
+
+
+    const renderContent = () => {
+        if (/^data:image\/[a-z]+;base64,/.test(profileData)) {
+
+            return (<img style={{ width: "100%", maxWidth: '150px', maxHeight: '150px', margin: '2px 0' }} width="50px" height="50px" src={profileData} alt="Base64" />);
+
+        } else {
+            return (<AccountCircleIcon style={{ fontSize: 150, color: profileData, margin: '2px 0' }} />);
         }
     };
-    handleFetchProfile();
-
-}, [email]);
 
 
-const renderContent = () => {
-    if (/^data:image\/[a-z]+;base64,/.test(profileData)) {
+    const toggleDrawer = (isopen) => () => {
+        onClose(isopen);
+    };
 
-        return (<img style={{ width:"100%", maxWidth: '150px', maxHeight:'150px', margin: '2px 0'  }}  width="50px" height="50px" src={profileData} alt="Base64" />);
+    const handlePinsButtonClick = () => {
+        setMyPinsModalOpen(true);
+    };
 
-    } else {
-        return(<AccountCircleIcon style={{ fontSize: 150, color: profileData, margin: '2px 0' }} />);
-    }
-};
+    const handleFriendsButtonClick = () => {
+        setFriendsModalOpen(true);
+    };
 
+    const [openSettings, setOpenSettings] = useState(false);
 
-const toggleDrawer = (isopen) => () => {
-    onClose(isopen);
-};
+    const handleOpenSettings = () => {
+        setOpenSettings(true);
+    };
 
-const handlePinsButtonClick = () => {
-    setMyPinsModalOpen(true);
-};
+    const handleCloseSettings = () => {
+        setOpenSettings(false);
+    };
 
-const handleFriendsButtonClick = () => {
-    setFriendsModalOpen(true);
-};
-
-const [openSettings, setOpenSettings] = useState(false);
-
-const handleOpenSettings = () => {
-    setOpenSettings(true);
-};
-
-const handleCloseSettings = () => {
-    setOpenSettings(false);
-};
-
-const list = () => (
-    <div
-        style={{
-            width: '40vw',
-            padding: '12px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            height: '80%',
-            background: 'white',
-        }}
-        role="presentation"
-        onClick={(e) => {
-            e.stopPropagation();
-        }}
-    >
-        <Typography variant="h5" gutterBottom>
-            PROFILE
-        </Typography>
-        <UsernameForm />
-        <Button onClick={handleOpenSettings} sx={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            margin: '10px',
-            padding: '8px',
-        }}
-            style={{ backgroundColor: 'white', color: 'black' }}>
-            <SettingsIcon />
-        </Button>
-        <Dialog open={openSettings} onClose={handleCloseSettings} fullWidth maxWidth="sm">
-            <DialogTitle sx={{ m: 0, p: 2 }}>
-                User Settings
-                <IconButton
-                    aria-label="close"
-                    onClick={handleCloseSettings}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent dividers>
-                <SettingsDialog />
-            </DialogContent>
-        </Dialog>
-
-        
-            {renderContent()}
-        
+    const list = () => (
         <div
             style={{
+                width: '40vw',
+                padding: '12px',
                 display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
+                flexDirection: 'column',
                 alignItems: 'center',
-                gap: '0',
-                margin: '0px 0',
+                height: '80%',
+                background: 'white',
             }}
-
-            //onKeyDown={toggleDrawer(false)}
-
-            <Typography variant="subtitle1" style={{ fontSize: '1rem', margin: '2px' }}>
-                200 PINS |
+            role="presentation"
+            onClick={(e) => {
+                e.stopPropagation();
+            }}
+           // onKeyDown={toggleDrawer(false)}
+        >
+            <Typography variant="h5" gutterBottom>
+                PROFILE
             </Typography>
-
-            <Typography variant="subtitle1" style={{ fontSize: '1rem', margin: '2px' }}>
-                200 LIKES |
-            </Typography>
-
-            <Typography variant="subtitle1" style={{ fontSize: '1rem', margin: '2px' }}>
-                2000 FRIENDS
-            </Typography>
-        </div>
-
-        <ListItem disablePadding>
-            <ButtonGroup style={{ width: '100%', justifyContent: 'center' }}>
-                <Button
-                    onClick={handleFriendsButtonClick}
-                    style={{
-                        background: 'linear-gradient(45deg, #4CAF50 30%, #2E7D32 90%)',
-                        borderRadius: 3,
-                        fontSize: '1rem',
-                        border: 3,
-                        color: 'white',
-                        height: 40,
-                        marginTop: 10,
-                        padding: '0 20px',
-                        boxShadow: '0 3px 5px 2px rgba(46, 125, 50, .3)',
-                    }}
-                >
-                    FRIENDS
-                </Button>
-                <Button
-                    onClick={handlePinsButtonClick}
-                    style={{
-                        background: 'linear-gradient(45deg, #4CAF50 30%, #2E7D32 90%)',
-                        borderRadius: 3,
-                        fontSize: '1rem',
-                        border: 3,
-                        color: 'white',
-                        marginTop: 10,
-                        height: 40,
-                        padding: '0 20px',
-                        boxShadow: '0 3px 5px 2px rgba(46, 125, 50, .3)',
-                    }}
-                >
-                    PINS
-                </Button>
-            </ButtonGroup>
-        </ListItem>
-        <Divider style={{ width: '100%', margin: '20px 0' }} />
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '8px' }}>
-            {/* Place your picture components here */}
-            <img src={Photo1} alt="Post 1" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo2} alt="Post 2" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo3} alt="Post 3" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo4} alt="Post 4" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo5} alt="Post 5" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo6} alt="Post 6" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo7} alt="Post 7" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo8} alt="Post 8" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            <img src={Photo9} alt="Post 9" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
-            {/* Add more pictures as needed */}
-        </div>
-    </div>
-);
-
-return (
-    <div>
-        <React.Fragment>
-            <SwipeableDrawer
-                anchor={drawerAnchor}
-                open={open}
-                onClose={toggleDrawer(false)}
-                onOpen={toggleDrawer(true)}
+            <Button
+                onClick={onClose}
+                variant="contained"
+                color="primary"
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    margin: '10px',
+                    padding: '8px',
+                    transition: 'background-color 0.3s',
+                    '&:hover': {
+                        backgroundColor: '#2196F3',
+                    },
+                }}
             >
-                {list()}
-            </SwipeableDrawer>
-            <MyPinsModal open={myPinsModalOpen} onClose={() => setMyPinsModalOpen(false)} />
-            <FriendsPin open={FriendsModalOpen} onClose={() => setFriendsModalOpen(false)} />
-        </React.Fragment>
-    </div>
-);
+                Log Out
+            </Button>
+
+            <Button onClick={handleOpenSettings} sx={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                margin: '10px',
+                padding: '8px',
+            }}
+                style={{ backgroundColor: 'white', color: 'black' }}>
+                <SettingsIcon />
+            </Button>
+            <Dialog open={openSettings} onClose={handleCloseSettings} fullWidth maxWidth="sm">
+                <DialogTitle sx={{ m: 0, p: 2 }}>
+                    User Settings
+                    <IconButton
+                        aria-label="close"
+                        onClick={handleCloseSettings}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent dividers>
+                    <SettingsDialog />
+                </DialogContent>
+            </Dialog>
+
+
+            {renderContent()}
+
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '0',
+                    margin: '0px 0',
+                }}
+            >
+                <Typography variant="subtitle1" style={{ fontSize: '1rem', margin: '2px' }}>
+                    200 PINS |
+                </Typography>
+
+                <Typography variant="subtitle1" style={{ fontSize: '1rem', margin: '2px' }}>
+                    200 LIKES |
+                </Typography>
+
+                <Typography variant="subtitle1" style={{ fontSize: '1rem', margin: '2px' }}>
+                    2000 FRIENDS
+                </Typography>
+            </div>
+
+            <ListItem disablePadding>
+                <ButtonGroup style={{ width: '100%', justifyContent: 'center' }}>
+                    <Button
+                        onClick={handleFriendsButtonClick}
+                        style={{
+                            background: 'linear-gradient(45deg, #4CAF50 30%, #2E7D32 90%)',
+                            borderRadius: 3,
+                            fontSize: '1rem',
+                            border: 3,
+                            color: 'white',
+                            height: 40,
+                            marginTop: 10,
+                            padding: '0 20px',
+                            boxShadow: '0 3px 5px 2px rgba(46, 125, 50, .3)',
+                        }}
+                    >
+                        FRIENDS
+                    </Button>
+                    <Button
+                        onClick={handlePinsButtonClick}
+                        style={{
+                            background: 'linear-gradient(45deg, #4CAF50 30%, #2E7D32 90%)',
+                            borderRadius: 3,
+                            fontSize: '1rem',
+                            border: 3,
+                            color: 'white',
+                            marginTop: 10,
+                            height: 40,
+                            padding: '0 20px',
+                            boxShadow: '0 3px 5px 2px rgba(46, 125, 50, .3)',
+                        }}
+                    >
+                        PINS
+                    </Button>
+                </ButtonGroup>
+            </ListItem>
+            <Divider style={{ width: '100%', margin: '20px 0' }} />
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '8px' }}>
+                {/* Place your picture components here */}
+                <img src={Photo1} alt="Post 1" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo2} alt="Post 2" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo3} alt="Post 3" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo4} alt="Post 4" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo5} alt="Post 5" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo6} alt="Post 6" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo7} alt="Post 7" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo8} alt="Post 8" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                <img src={Photo9} alt="Post 9" style={{ width: '150px', height: '150px', borderRadius: '0px' }} />
+                {/* Add more pictures as needed */}
+            </div>
+        </div>
+    );
+
+    return (
+        <div>
+            <React.Fragment>
+                <SwipeableDrawer
+                    anchor={drawerAnchor}
+                    open={open}
+                    onClose={toggleDrawer(false)}
+                    onOpen={toggleDrawer(true)}
+                >
+                    {list()}
+                </SwipeableDrawer>
+                <MyPinsModal open={myPinsModalOpen} onClose={() => setMyPinsModalOpen(false)} />
+                <FriendsPin open={FriendsModalOpen} onClose={() => setFriendsModalOpen(false)} />
+            </React.Fragment>
+        </div>
+    );
 };
 
 const UserProfile = ({ onClose }) => {
-const [drawerOpen, setDrawerOpen] = useState(true);
-const [myPinsModalOpen, setMyPinsModalOpen] = useState(false);
+    const [drawerOpen, setDrawerOpen] = useState(true);
+    const [myPinsModalOpen, setMyPinsModalOpen] = useState(false);
 
-const handlePinsButtonClick = () => {
-    // Close the drawer and trigger the callback to open the MyPins modal
-    setDrawerOpen(true);
-    setMyPinsModalOpen(false);
-    onClose();
-};
+    const handlePinsButtonClick = () => {
+        // Close the drawer and trigger the callback to open the MyPins modal
+        setDrawerOpen(true);
+        setMyPinsModalOpen(false);
+        onClose();
+    };
 
-return (
-    <div>
-        <SwipeableTemporaryDrawer open={drawerOpen} onClose={() => { setDrawerOpen(true); onClose(); }} />
-    </div>
-);
+    return (
+        <div>
+            <SwipeableTemporaryDrawer open={drawerOpen} onClose={() => { setDrawerOpen(true); onClose(); }} />
+        </div>
+    );
 };
 
 export default UserProfile;
